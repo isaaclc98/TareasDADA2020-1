cde7a47a0e79c34100214b7a2290141cdd5e891d
isaaclc98 <isaaclc98@gmail.com>
2019-08-26 08:51:29 -0500

    tarea 7 4funciones

---
+++ tarea7/tarea7.py
@@ -0,0 +1,18 @@
+#tarea 7 
+#4 funciones basicas en python (suma,resta, multiplicacion y division)
+def add(num1,num2):
+    return num1 + num2
+
+def substract(num1,num2):
+    return num1 - num2
+
+def multiply(num1,num2):
+    return num1 * num2
+
+def divide(num1,num2):
+    return num1 / num2
+
+print(add(3,5))
+print(substract(10,4))
+print(multiply(7,-4))
+print(divide(45,5))

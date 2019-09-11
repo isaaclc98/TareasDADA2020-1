710e81ef651ba131341cbd4b447b8c3c1adf0b9c
isaaclc98 <isaaclc98@gmail.com>
2019-09-10 21:26:59 -0500

    Serie Taylo Clase

--- ProgClase/serieTaylor.py
+++ ProgClase/serieTaylor.py
@@ -9,3 +9,18 @@ def potencia(base,exp):
     else:
         return base * potencia(base,exp-1)
 
+def seno(x):
+    conta=1
+    result = 0 
+    for i in range (0,10):
+        
+        if i%2 == 0:
+            result = result + (potencia(x,conta)/factorial(conta))
+        else:
+            result = result - (potencia(x,conta)/factorial(conta))
+            
+        #print(conta)
+        conta = conta +2
+    return result
+
+print(seno(10))

65219c7a6aa674babc6663df21696212aaa42b43
isaaclc98 <isaaclc98@gmail.com>
2019-10-04 17:27:03 -0500

    N numero non

--- Tarea13/Fn-2.py
+++ Tarea13/Fn-2.py
@@ -2,10 +2,11 @@
 Version Fn-2
 Crear una funcion que retorne el enesimo numero non
 '''
-def nNon(num):
-    return num*2-2
 
-print(nNon(1))#0
-print(nNon(3))#4
-print(nNon(100))#198
-print(nNon(1298734))#259744
+def nNon(num):
+    return num*2-1
+
+print(nNon(1))#1
+print(nNon(3))#5
+print(nNon(100))#199
+print(nNon(1298734))#2597467

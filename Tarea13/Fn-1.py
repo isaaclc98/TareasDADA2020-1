d4b6ed3daa6a50613c1bd500e4969b86c47ddebe
isaaclc98 <isaaclc98@gmail.com>
2019-10-04 17:22:36 -0500

    N numero par

---
+++ Tarea13/Fn-1.py
@@ -0,0 +1,11 @@
+'''
+Version Fn-1
+Crear una funcion que retorne el enesimo numero par
+'''
+def nPar(num):
+    return num*2-2
+
+print(nPar(1))#0
+print(nPar(3))#4
+print(nPar(100))#198
+print(nPar(1298734))#259744
